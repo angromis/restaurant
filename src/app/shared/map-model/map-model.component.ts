e1d85ec1a8add33c8c5a9360b70bcf80742f070f
angromis <andregromis@gmail.com>
2021-02-18 01:24:58 +0200

    aaaa

--- src/app/shared/map-model/map-model.component.ts
+++ src/app/shared/map-model/map-model.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -6,8 +6,10 @@ import { ModalController } from '@ionic/angular';
   templateUrl: './map-model.component.html',
   styleUrls: ['./map-model.component.scss'],
 })
-export class MapModelComponent implements OnInit, AfterViewInit {
-  @ViewChild('map') mapElementRef: ElementRef;
+export class MapModelComponent implements OnInit, AfterViewInit,OnDestroy {
+  @ViewChild('map',null) mapElementRef: ElementRef;
+  clickListener: any;
+  googleMaps:any;
 
   constructor(private modalCtrl: ModalController, private renderer: Renderer2) { }
 
@@ -16,6 +18,7 @@ export class MapModelComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(){
 
     this.getGoogleMaps().then(googleMaps => {
+      this.googleMaps = googleMaps;
       const mapEl = this.mapElementRef.nativeElement;
       const map =  new googleMaps.Map(mapEl, {
         center: {lat:38.0482, lng: 23.7558}, 
@@ -25,7 +28,7 @@ export class MapModelComponent implements OnInit, AfterViewInit {
         this.renderer.addClass(mapEl, 'visible');
       });
 
-      map.addListener('click', event => {
+      this.clickListener = map.addListener('click', event => {
         const selectedCoords = {lat: event.latLng.lat(), 
                                 lng: event.latLng.lng()
         };
@@ -65,6 +68,11 @@ export class MapModelComponent implements OnInit, AfterViewInit {
 
       };
     })
+  }
+  ngOnDestroy(){
+    this.googleMaps.event.removeListener(this.clickListener);
+    console.log('trigered')
+
   }
 
 }
--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModelComponent } from '../../map-model/map-model.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from 'src/app/admin/restaurants/location.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -11,6 +13,8 @@ import { map } from 'rxjs/operators';
 })
 export class LocationPickerComponent implements OnInit {
 
+  selectedLocationImage:string;
+  isLoading = false;
   constructor(private modalCtrl: ModalController, private http: HttpClient) { }
 
   ngOnInit() {}
@@ -21,8 +25,21 @@ export class LocationPickerComponent implements OnInit {
         if(!modalData.data){
           return
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe((address) =>{
-          console.log(address);
+        const pickedLocation: PlaceLocation = {
+          lat:modalData.data.lat,
+          lng:modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null
+        };
+        this.isLoading = true;
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(switchMap(address => {
+          pickedLocation.address = address;
+          return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+
+        })).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          this.selectedLocationImage = staticMapImageUrl;
+          this.isLoading = false;
         });
       });
       modalEl.present();
@@ -36,6 +53,12 @@ export class LocationPickerComponent implements OnInit {
      }
      return geoData.results[0].formatted_address;
     }));
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number){
+   // return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&markers=color:blue%7Clabel:S%7C${lat},${lng}&key=AIzaSyAQFMSBMQmkR2iR-PZ9prufnLRZc-1INMo`
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&markers=color:orange%7Clabel:S%7C11211%7C11206%7C11222&key=AIzaSyAQFMSBMQmkR2iR-PZ9prufnLRZc-1INMo`
+   // return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&key=AIzaSyAQFMSBMQmkR2iR-PZ9prufnLRZc-1INMo`
   }
 
 }
